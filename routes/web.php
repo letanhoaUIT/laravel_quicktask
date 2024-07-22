8af37fc4b793aae0a22b41b48a425b93231c7b84
root <root@Hoa>
2024-07-22 10:05:34 +0700

    feat: update auth

--- routes/web.php
+++ routes/web.php
@@ -1,39 +1,20 @@
 <?php
 
-use App\Http\Controllers\TaskController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
-
-// Đăng nhập bằng ID người dùng với ID = 2
-Auth::loginUsingId(2);
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/users/index', [UserController::class, 'index'])->name('users.index');
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-Route::get('/users/edit', [UserController::class, 'edit'])->name('users.edit');
-Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
-Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
-Route::delete('/users/{user}', [UserController::class, 'delete'])->name('users.delete');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::resource('/tasks', TaskController::class); //cach nhanh so voi cac lenh tren
-
-Route::get('/users/index', [UserController::class, 'index'])->name('users.index');
-Route::get('/users/create', [UserController::class, 'create'])
-    ->name('users.create')
-    ->middleware([
-        'admin',
-    ]);
-Route::get('/users/edit', [UserController::class, 'edit'])->name('users.edit');
-Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
-Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
-Route::delete('/users/{user}', [UserController::class, 'delete'])->name('users.delete');
-
-
-Route::resource('/tasks', TaskController::class); //cach nhanh so voi cac lenh tren
+require __DIR__.'/auth.php';
