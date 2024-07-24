116f48e0939d9fa4ca982c5f1745024ee7216e6d
root <root@Hoa>
2024-07-24 12:04:48 +0700

    feat: update Bower & Asset Bundling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,9 @@
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        {{-- <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" /> --}}
+        {{-- <link rel="stylesheet" href="{{ asset('bower_components/Font-Awesome/css/fontawesome.min.css') }}"> --}}
+        <link rel="stylesheet" href="{{ asset('bower_components/font-awesome/css/font-awesome.min.css') }}">
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
