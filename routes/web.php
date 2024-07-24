bdd4e2153412aef84ed6663a6619622d68274e7e
root <root@Hoa>
2024-07-24 23:56:48 +0700

    Eloquent and Query Builder

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,10 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 // Đăng nhập bằng ID người dùng với ID = 2
-Auth::loginUsingId(2);
+//Auth::loginUsingId(2);
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,29 +18,23 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/users/index', [UserController::class, 'index'])->name('users.index');
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-Route::get('/users/edit', [UserController::class, 'edit'])->name('users.edit');
-Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
-Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
-Route::delete('/users/{user}', [UserController::class, 'delete'])->name('users.delete');
-
-
 Route::middleware(['auth', 'admin'])->group(function () {
-    Route::get('/user', [UserController::class, 'index'])->name('users.index');
-    Route::get('/user/edit', [UserController::class, 'edit'])->name('users.edit');
-    Route::get('/user/create', [UserController::class, 'create'])->name('users.create');
-    Route::get('/user/{user}', [UserController::class, 'show'])->name('users.show');
-    Route::delete('/user/delete', [UserController::class, 'delete'])->name('users.delete');
-    Route::put('/user/update', [UserController::class, 'update'])->name('users.update');
-    Route::post('/user', [UserController::class, 'store'])->name('users.store');
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
+    Route::delete('/users/delete', [UserController::class, 'delete'])->name('users.delete');
+    Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
 });
 
 Route::resource('task', TaskController::class);
